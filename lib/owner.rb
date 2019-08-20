b68bfcdced90e6b793eab81983e19538ac8c9e96
jose luis <cortezluisjose@gmail.com>
2019-08-20 02:25:56 +0000

    Automatically backed up by Learn

---
+++ lib/owner.rb
@@ -0,0 +1,12 @@
+class Owner
+  attr_accessor :pets, :name 
+  attr_reader :species 
+  
+  @@all = []
+ 
+ def initialize(species)
+   @species = species
+   @pets = {cats:[], dogs: []}
+  end 
+  
+end 
